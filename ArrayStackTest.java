aa3918c8814598d03822f326c63497e3c8e655fa
nakarle <89620780+nakarle@users.noreply.github.com>
2021-10-11 20:38:46 -0700

    Static reference instead of instance

--- ArrayStackTest.java
+++ ArrayStackTest.java
@@ -2,22 +2,22 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ArrayStackTest {
         public static void main(String args[]){
-            StackInterface<Integer> testStack = new ResizableArrayStack<>();
-            testStack.push(testStack.evaluatePostfix("ae+bd-/"));
+            ResizableArrayStack<Integer> testStack = new ResizableArrayStack<>();
+            testStack.push(ResizableArrayStack.evaluatePostfix("ae+bd-/"));
             System.out.println(testStack.peek());
-            testStack.push(testStack.evaluatePostfix("abc*d*-"));
+            testStack.push(ResizableArrayStack.evaluatePostfix("abc*d*-"));
             System.out.println(testStack.peek());
-            testStack.push(testStack.evaluatePostfix("abc-/d*"));
+            testStack.push(ResizableArrayStack.evaluatePostfix("abc-/d*"));
             System.out.println(testStack.peek());
-            testStack.push(testStack.evaluatePostfix("ebca^*+d-"));
+            testStack.push(ResizableArrayStack.evaluatePostfix("ebca^*+d-"));
             System.out.println(testStack.peek());
         }
     @org.junit.jupiter.api.Test
     /** Test case #1
      */
     void evaluatePostfix() {
-        StackInterface<Integer> testStack = new ResizableArrayStack<>();
-        testStack.push(testStack.evaluatePostfix("ae+bd-/"));
+        ResizableArrayStack<Integer> testStack = new ResizableArrayStack<>();
+        testStack.push(ResizableArrayStack.evaluatePostfix("ae+bd-/"));
         int test = testStack.peek();
         assertEquals(-4, test);
     }
@@ -25,8 +25,8 @@ class ArrayStackTest {
     /** Test case #2
      */
     void evaluatePostfix2() {
-        StackInterface<Integer> testStack = new ResizableArrayStack<>();
-        testStack.push(testStack.evaluatePostfix("abc*d*-"));
+        ResizableArrayStack<Integer> testStack = new ResizableArrayStack<>();
+        testStack.push(ResizableArrayStack.evaluatePostfix("abc*d*-"));
         int test = testStack.peek();
         assertEquals(-58, test);
     }
@@ -35,8 +35,8 @@ class ArrayStackTest {
     /** Test case #3
      */
     void evaluatePostfix3() {
-        StackInterface<Integer> testStack = new ResizableArrayStack<>();
-        testStack.push(testStack.evaluatePostfix("abc-/d*"));
+        ResizableArrayStack<Integer> testStack = new ResizableArrayStack<>();
+        testStack.push(ResizableArrayStack.evaluatePostfix("abc-/d*"));
         int test = testStack.peek();
         assertEquals(-10, test);
     }
@@ -45,8 +45,8 @@ class ArrayStackTest {
     /** Test case #4
      */
     void evaluatePostfix4() {
-        StackInterface<Integer> testStack = new ResizableArrayStack<>();
-        testStack.push(testStack.evaluatePostfix("ebca^*+d-"));
+        ResizableArrayStack<Integer> testStack = new ResizableArrayStack<>();
+        testStack.push(ResizableArrayStack.evaluatePostfix("ebca^*+d-"));
         int test = testStack.peek();
         assertEquals(49, test);
     }
@@ -55,8 +55,8 @@ class ArrayStackTest {
     /** Test case #5 Input with spaces
      */
     void evaluatePostfix5() {
-        StackInterface<Integer> testStack = new ResizableArrayStack<>();
-        testStack.push(testStack.evaluatePostfix("a e+  b d- / "));
+        ResizableArrayStack<Integer> testStack = new ResizableArrayStack<>();
+        testStack.push(ResizableArrayStack.evaluatePostfix("a e+  b d- / "));
         int test = testStack.peek();
         assertEquals(-4, test);
     }
@@ -65,24 +65,24 @@ class ArrayStackTest {
     /** Test case #6 Input with an invalid variable
      */
     void evaluatePostfix6() {
-        StackInterface<Integer> testStack = new ResizableArrayStack<>();
-        assertThrows(IllegalStateException.class, () -> testStack.evaluatePostfix("zxy+-"));
+        ResizableArrayStack<Integer> testStack = new ResizableArrayStack<>();
+        assertThrows(IllegalStateException.class, () -> ResizableArrayStack.evaluatePostfix("zxy+-"));
         }
 
     @org.junit.jupiter.api.Test
     /** Test case #7 Input with an invalid variable (number)
      */
     void evaluatePostfix7() {
-        StackInterface<Integer> testStack = new ResizableArrayStack<>();
-        assertThrows(IllegalStateException.class, () -> testStack.evaluatePostfix("12+"));
+        ResizableArrayStack<Integer> testStack = new ResizableArrayStack<>();
+        assertThrows(IllegalStateException.class, () -> ResizableArrayStack.evaluatePostfix("12+"));
         }
 
     @org.junit.jupiter.api.Test
     /** Test case #6 Input with uppercase
      */
     void evaluatePostfix8() {
-        StackInterface<Integer> testStack = new ResizableArrayStack<>();
-        testStack.push(testStack.evaluatePostfix("AE+bD-/"));
+        ResizableArrayStack<Integer> testStack = new ResizableArrayStack<>();
+        testStack.push(ResizableArrayStack.evaluatePostfix("AE+bD-/"));
         int test = testStack.peek();
         assertEquals(-4, test);
     }
